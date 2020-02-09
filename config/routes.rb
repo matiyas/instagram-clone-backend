08c1b0c1b9e75d3d987dded593fe536a613a43e6
Damian Matyjaszek <tougepl@gmail.com>
2020-02-09 13:33:34 +0100

    Configure devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: :all
+
+  defaults format: :json do
+    devise_scope :user do
+      post 'api/v1/auth/sign_in', to: 'devise/sessions#create'
+      delete 'api/v1/auth/sign_out', to: 'devise/sessions#destroy'
+      patch 'api/v1/auth/password', to: 'devise/passwords#update'
+      put 'api/v1/auth/password', to: 'devise/passwords#update'
+      post 'api/v1/auth/password', to: 'devise/passwords#create'
+      patch 'api/v1/auth/user', to: 'devise/registrations#update'
+      put 'api/v1/auth/user', to: 'devise/registrations#update'
+      delete 'api/v1/auth/user', to: 'devise/registrations#destroy'
+      post 'api/v1/auth/user', to: 'devise/registrations#create'
+      get 'api/v1/auth/unlock', to: 'devise/unlocks#show'
+      post 'api/v1/auth/unlock', to: 'devise/unlocks#create'
+    end
+  end
 end
